d1a3981a3d30cd9106be3c8688c286aece0e7d8d
poswalsameer <poswalsameer2003@gmail.com>
2024-05-25 18:48:45 +0530

    setup of nextjs and made the first layout

---
+++ shinyfortune/src/app/page.tsx
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <>
+
+      <div className="h-screen w-full bg-black flex flex-col justify-center items-center">
+
+        <h1 className="my-6 text-5xl text-white font-extrabold" > SHINY FORTUNE </h1>
+
+        <div className="my-4 h-[30rem] w-[80rem] bg-blue-950 rounded-xl flex flex-row justify-between items-center" >
+
+          <div className=" ml-10 h-[26rem] w-[30rem] bg-green-900 rounded-2xl">
+
+          </div>
+
+          <div className="mr-10 h-[26rem] w-[32rem] bg-green-900 rounded-2xl">
+
+          </div>
+
+        </div>
+
+        
+      </div>
+
+    </>
+  );
+}
